32871bbc0941b0f957bbbb5391e0f6de3a627811
vincent.mathon <vincent.mathonj@saagie.com>
2022-10-26 12:31:22 +0200

    introduce protobuf messages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
 
 plugins {
 	id("org.springframework.boot") version "2.7.5"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+	id("idea")
+	id("java")
+	id("com.google.protobuf") version "0.9.1"
 }
 
 group = "com.example"
@@ -22,6 +26,10 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	implementation("com.google.protobuf:protobuf-java:3.21.8")
+	implementation("com.google.protobuf:protobuf-java-util:3.21.8")
+	implementation("io.grpc:grpc-stub:1.50.2")
+	implementation("io.grpc:grpc-protobuf:1.50.2")
 }
 
 tasks.withType<KotlinCompile> {
@@ -29,8 +37,37 @@ tasks.withType<KotlinCompile> {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "17"
 	}
+	dependsOn("generateProto")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+	dependsOn("generateProto")
 }
+
+protobuf {
+	protoc {
+		// The artifact spec for the Protobuf Compiler
+		artifact = "com.google.protobuf:protoc:3.21.8"
+	}
+	plugins {
+		// Optional: an artifact spec for a protoc plugin, with "grpc" as
+		// the identifier, which can be referred to in the "plugins"
+		// container of the "generateProtoTasks" closure.
+		id("grpc") {
+			artifact = "io.grpc:protoc-gen-grpc-java:1.50.2"
+		}
+	}
+	generateProtoTasks {
+		ofSourceSet("main").forEach {
+			it.plugins {
+				// Apply the "grpc" plugin whose spec is defined above, without
+				// options. Note the braces cannot be omitted, otherwise the
+				// plugin will not be added. This is because of the implicit way
+				// NamedDomainObjectContainer binds the methods.
+				id("grpc") { }
+			}
+		}
+	}
+}
+
--- src/main/kotlin/com/example/demo/RabbitMQRouteBuilder.kt
+++ src/main/kotlin/com/example/demo/RabbitMQRouteBuilder.kt
@@ -1,30 +1,41 @@
 package com.example.demo
 
+import com.google.protobuf.util.Timestamps
 import org.apache.camel.builder.RouteBuilder
 import org.springframework.stereotype.Component
-import kotlin.random.Random
+import java.util.*
 
 @Component
 class RabbitMQRouteBuilder : RouteBuilder() {
+    internal fun buildMessage(date: Date): SyncOrder {
+        val yaml = "I am yaml"
+        return SyncOrder.newBuilder()
+            .setId(10000L)
+            .setType(SyncOrder.SyncOrderType.JobCreatedSyncOrder)
+            .setCreatedAt(Timestamps.fromMillis(date.toInstant().toEpochMilli()))
+            .setDeployment(
+                SyncOrder.Deployment.newBuilder()
+                    .setLabel("PythonJob")
+                    .setYaml(yaml)
+            )
+            .build()
+    }
 
     override fun configure() {
         from("timer://myTimer?period=2000")
-            .setBody()
-            .spel("Hello World Camel fired at #{request.headers['firedTime']}")
+            .process {
+                val date = it.getIn().getHeader("firedTime", Date::class.java)
+                val message = buildMessage(date)
+                it.getIn().setBody(message.toByteArray(), ByteArray::class.java)
+            }
             .to("log:info")
             .to("spring-rabbitmq:CPGateway?routingKey=CPGatewayOrders")
 
         from("spring-rabbitmq:CPGateway?queues=CPGatewayQueue")
             .process {
-                if(RabbitMQRouteObject.random.nextInt() % 2 == 0)
-                    throw Exception("Random failure")
-                else
-                    log.info("Processing message successfully")
+                val messageAsBytes = it.getIn().getBody(ByteArray::class.java)
+                it.getIn().setBody(SyncOrder.parseFrom(messageAsBytes), String::class.java)
             }
             .to("log:warn")
     }
-}
-
-object RabbitMQRouteObject {
-    internal val random = Random(0)
 }
