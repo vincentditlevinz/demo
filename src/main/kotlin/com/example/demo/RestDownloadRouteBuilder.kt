8f6e57d4efdec664ed03c7c7274f11f146414c88
vincent.mathon <vincent.mathonj@saagie.com>
2022-11-02 11:31:44 +0100

    download from minio

--- src/main/kotlin/com/example/demo/RestDownloadRouteBuilder.kt
+++ src/main/kotlin/com/example/demo/RestDownloadRouteBuilder.kt
@@ -1,6 +1,8 @@
 package com.example.demo
 
+import io.minio.ListObjectsArgs
 import org.apache.camel.builder.RouteBuilder
+import org.apache.camel.component.minio.MinioConstants
 import org.springframework.stereotype.Component
 
 @Component("RestDownloadRouteBuilder")
@@ -10,15 +12,21 @@ class RestDownloadRouteBuilder : RouteBuilder() {
         restConfiguration().component("servlet")
             .host("localhost").port(8080)
 
-        rest("/download")
+        rest("/download/{path}")
             .get()
             .routeId("downloadApi")
             .produces(org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE)
-            .to("direct:download")
+            .to("direct:downloadMinio")
 
-        from("direct:download")
-            .routeId("serveFiles")
-            .pollEnrich("file:src/main/resources/assetsForDownload?noop=true")
-            .setHeader("Content-Disposition").spel("attachment;filename=#{headers.CamelFileName}")
+        from("direct:downloadMinio")
+            .routeId("serveFromMinio")
+            .setBody { ListObjectsArgs.builder()
+                .bucket("external-technology-scripts")
+                .recursive(true) }
+            .process {
+                it.getIn().setHeader(MinioConstants.OBJECT_NAME, it.getIn().getHeader("path"))
+            }
+            .pollEnrich("minio://external-technology-scripts?operation=getObject&pojoRequest=true", 10000)
+            .setHeader("Content-Disposition").spel("attachment;filename=#{headers.CamelMinioObjectName}")
     }
 }
