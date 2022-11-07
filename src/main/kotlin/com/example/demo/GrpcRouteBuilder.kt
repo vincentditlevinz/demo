f60bfc5e8f977a1be89273e83325334289c73eb8
vincent.mathon <vincent.mathonj@saagie.com>
2022-11-07 18:17:11 +0100

    I can use static routing finally !

--- src/main/kotlin/com/example/demo/GrpcRouteBuilder.kt
+++ src/main/kotlin/com/example/demo/GrpcRouteBuilder.kt
@@ -2,15 +2,11 @@ package com.example.demo
 
 import com.google.protobuf.util.Timestamps
 import io.grpc.Status
-import io.grpc.StatusRuntimeException
 import org.apache.camel.Body
-import org.apache.camel.ExchangeProperties
-import org.apache.camel.Header
 import org.apache.camel.builder.RouteBuilder
 import org.springframework.stereotype.Component
 import java.time.Instant
 import java.util.*
-import java.util.Map
 
 @Component("GrpcRouteBuilder")
 class GrpcRouteBuilder : RouteBuilder() {
@@ -33,34 +29,13 @@ class GrpcRouteBuilder : RouteBuilder() {
         throw Status.UNIMPLEMENTED.withDescription("Unimplemented end-point").asRuntimeException()
     }
 
-
-    /* Static router cannot be used with Kotlin because of 'when' reserved keyword, we must use a dynamic router which is a bit more involved */
-    fun routing(@Header("CamelGrpcMethodName") grpcMethodName: String, @ExchangeProperties properties: Map<String, String>): String? {
-        val route = properties.getOrDefault("route", run {
-            if (grpcMethodName == "getSyncOrders") {
-                "direct:getSyncOrders"
-            } else {
-                "direct:error"
-            }
-        })
-        properties.put("route", "end")
-        return if(route == "end")
-            null
-        else
-            route
-    }
-
     override fun configure() {
         from("grpc://0.0.0.0:9991/com.example.demo.SynchronizationService?consumerStrategy=PROPAGATION")
             .routeId("grpcServiceRouter")
-            .dynamicRouter(method(GrpcRouteBuilder::class.java, "routing"))
-
-        from("direct:getSyncOrders")
-            .routeId("getSyncOrders")
-            .bean("GrpcRouteBuilder", "buildResponse")
-
-        from("direct:error")
-            .routeId("grpcError")
-            .bean("GrpcRouteBuilder", "buildError")
+            .choice()
+            .`when`(header("CamelGrpcMethodName").isEqualTo("getSyncOrders"))// Note the `when` because when is a reserved keyword in Kotlin
+                .bean("GrpcRouteBuilder", "buildResponse")
+            .otherwise()
+                .bean("GrpcRouteBuilder", "buildError")
     }
 }
